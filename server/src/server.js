68630cff299f1e87ff279dfddb9fc3f89663b313
Gautamikahar <gautami.kahar@gmail.com>
2026-07-24 16:00:16 +0530

    add jwt

--- server/src/server.js
+++ server/src/server.js
@@ -1,10 +1,13 @@
 import dotenv from "dotenv";
 import app from "./app.js";
 import connectDB from "./config/db.js";
-
+import studentRoutes from "./routes/studentRoutes.js";
 
 dotenv.config();
-
+app.use(
+  "/api/student",
+  studentRoutes
+);
 const PORT = process.env.PORT || 5000;
 
 connectDB();
